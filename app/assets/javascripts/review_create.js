92ee3c86fe76b240b50a74f7c41dddc7df88adf5
yumamitani <ymsy.w.life3@icloud.com>
2019-12-24 23:49:02 +0900

    fix post_function

--- app/assets/javascripts/review_create.js
+++ app/assets/javascripts/review_create.js
@@ -1,14 +1,20 @@
 $(function(){
-  $(".image_area__contents--box").hover(
-    function(){
-    $(this).css({"backgroundColor": "#00a0de"}, 5000)
-    },
-    function(){
-    $(this).css({backgroundColor: "white"}, 2000)
+  $( document ).on('turbolinks:load', function() {
+    function readURL(input) {
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+   
+        reader.onload = function (e) {
+          $('.image').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+      }
     }
-  );
-
-  $(".file-area").change(function(){
-    
-  })
+   
+    $(".post-image").change(function(){
+      $('.image').removeClass('hidden');
+      $('.icon').remove();
+      readURL(this);
+    });
+  });
 });
